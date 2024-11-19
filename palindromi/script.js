a81871afdec8727447119095fbb19fc19a315bbe
fabioferrero90 <fabio.ff90@gmail.com>
2024-11-19 13:42:24 +0100

    feat: add check for palindrome words

---
+++ palindromi/script.js
@@ -0,0 +1,30 @@
+let correct;
+
+// Controllo in un ciclo che la parola inserita sia verificabile
+while (!correct){
+
+  // Chiedo la parola da esaminare all'utente
+  let promptedWord = prompt("Scrivi una parola per testare se è palindroma:")
+
+  // Verifico che l'utente abbia inserito una parola senza spazi
+  if (!promptedWord.includes(" ")){
+    correct = true
+    isPalindromic(promptedWord) ? (message = "La parola è palindroma") : (message = "La parola non è palindroma");
+  } else {
+    message = "Devi inserire una sola parola!"
+  }
+
+  // Comunico l'esito
+  alert(message)
+}
+
+
+// Creo una funzione che controlla se la parola è uguale anche al contrario (Palindroma)
+function isPalindromic(string){
+  const reversestring = string.split("").reverse().join("")
+  if(reversestring === string){
+    return true
+  } else {
+    return false
+  }
+}
