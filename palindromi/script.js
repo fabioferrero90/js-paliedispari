901a0f488e6f974b248e1b18b0da6bb9125ba40b
fabioferrero90 <fabio.ff90@gmail.com>
2024-11-20 11:14:04 +0100

    fix: palindrome code refactor

--- palindromi/script.js
+++ palindromi/script.js
@@ -1,28 +1,28 @@
 let correct;
 
 // Controllo in un ciclo che la parola inserita sia verificabile
-while (!correct){
-
+while (!correct) {
   // Chiedo la parola da esaminare all'utente
-  let promptedWord = prompt("Scrivi una parola per testare se è palindroma:").toLowerCase()
-
+  let promptedWord = prompt("Scrivi una parola per testare se è palindroma:").toLowerCase();
+  let message = 'La parola non è palindroma';
   // Verifico che l'utente abbia inserito una parola senza spazi
-  if (promptedWord && !promptedWord.includes(" ")){
-    correct = true
-    isPalindromic(promptedWord) ? (message = "La parola è palindroma") : (message = "La parola non è palindroma");
+  if (promptedWord && !promptedWord.includes(" ")) {
+    correct = true;
+    if (isPalindromic(promptedWord)){
+      message = "La parola è palindroma"
+    }
   } else if (!promptedWord) {
-    message = "Devi inserire una parola!"  
+    message = "Devi inserire una parola!";
   } else {
-    message = "Devi inserire una sola parola!"
+    message = "Devi inserire una sola parola!";
   }
 
   // Comunico l'esito
-  alert(message)
+  alert(message);
 }
 
-
 // Creo una funzione che controlla se la parola è uguale anche al contrario (Palindroma)
-function isPalindromic(string){
-  const reversestring = string.split("").reverse().join("")
-  return reversestring === string
+function isPalindromic(string) {
+  const reverseString = string.split("").reverse().join("");
+  return reverseString === string;
 }
